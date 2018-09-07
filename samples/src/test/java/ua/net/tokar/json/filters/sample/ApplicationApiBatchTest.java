c11d0de895c559a450dea5cfd890fa3a0d96a7fa
andrey.samuleyev <andrey.samuleyev@worldapp.com>
2018-09-07 18:20:07 +0300

    Respond with 'Bad Request' status on incorrect body content.

--- samples/src/test/java/ua/net/tokar/json/filters/sample/ApplicationApiBatchTest.java
+++ samples/src/test/java/ua/net/tokar/json/filters/sample/ApplicationApiBatchTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashMap;
@@ -40,5 +42,15 @@ public class ApplicationApiBatchTest {
         assertThat( body, hasJsonPath( "$.users[0].name", is( "boss" ) ) );
         assertThat( body, hasJsonPath( "$.users[1].name", is( "cat" ) ) );
 
+    }
+
+    @Test
+    public void badRequestTest() {
+        ResponseEntity<String> response = restTemplate.postForEntity(
+                "/batch",
+                "incorrectBody",
+                String.class
+        );
+        assertThat( response.getStatusCode(), is( HttpStatus.BAD_REQUEST ) );
     }
 }
